07591869dc1febefb7039d07a15c9b70050f53de
Vincent Masselis <vincent@equisense.fr>
2017-03-16 15:50:22 +0100

    Remove unused file

--- src/main/kotlin/com/equisense/rxkotlinbleandroid/internal/Disposable+rx.kt
+++
@@ -1,9 +0,0 @@
-package com.equisense.rxkotlinbleandroid.internal
-
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
-
-fun Disposable.addTo(compositeSubscription: CompositeDisposable): Disposable {
-    compositeSubscription.add(this)
-    return this
-}
