e07a8a5a439140a586fcbd2894370ec3ec65ea5d
Vincent Masselis <vincent@equisense.fr>
2017-08-11 16:48:04 +0200

    Fire BluetoothIsTurnedOff() exception when bluetooth is turned off while scanning

--- src/main/kotlin/com/vincentmasselis/rxbluetoothkotlin/BleScanner.kt
+++ src/main/kotlin/com/vincentmasselis/rxbluetoothkotlin/BleScanner.kt
@@ -2,18 +2,21 @@ package com.vincentmasselis.rxbluetoothkotlin
 
 import android.Manifest
 import android.annotation.TargetApi
+import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Context
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.os.Build
 import android.support.v4.content.ContextCompat
+import com.vincentmasselis.rxbluetoothkotlin.internal.toObservable
 import io.reactivex.BackpressureStrategy
 import io.reactivex.Completable
 import io.reactivex.Flowable
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
+import io.reactivex.disposables.CompositeDisposable
 import no.nordicsemi.android.support.v18.scanner.*
 import java.util.concurrent.TimeUnit
 
@@ -40,27 +43,42 @@ fun BluetoothManager.rxScan(context: Context, scanArgs: Pair<List<ScanFilter>, S
                             val callback = object : ScanCallback() {
                                 @TargetApi(Build.VERSION_CODES.LOLLIPOP)
                                 override fun onScanResult(callbackType: Int, result: ScanResult) {//TODO Handle callbackType
-                                    if(downStream.isCancelled.not()) downStream.onNext(result)
+                                    if (downStream.isCancelled.not()) downStream.onNext(result)
                                 }
 
                                 override fun onScanFailed(errorCode: Int) {
-                                    if(downStream.isCancelled.not()) downStream.onError(ScanFailedException(errorCode))
+                                    if (downStream.isCancelled.not()) downStream.onError(ScanFailedException(errorCode))
                                 }
                             }
                             val scanner = BluetoothLeScannerCompat.getScanner()
 
+
+                            val disposables = CompositeDisposable()
+
+                            IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
+                                    .toObservable(context)
+                                    .subscribe { (_, intent) ->
+                                        val state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR)
+                                        when (state) {
+                                            BluetoothAdapter.STATE_TURNING_OFF, BluetoothAdapter.STATE_OFF -> downStream.onError(BluetoothIsTurnedOff())
+                                            else -> {
+                                            }
+                                        }
+                                    }
+                                    .let { disposables.add(it) }
+
+                            flushEvery?.run {
+                                Observable
+                                        .interval(flushEvery.first, flushEvery.second, AndroidSchedulers.mainThread())
+                                        .subscribe { scanner.flushPendingScanResults(callback) }
+                                        .let { disposables.add(it) }
+                            }
+
                             if (scanArgs != null) scanner.startScan(scanArgs.first, scanArgs.second, callback)
                             else scanner.startScan(callback)
 
-                            var flushEveryDisp: Disposable? = null
-                            flushEvery?.run {
-                                flushEveryDisp = Observable
-                                        .interval(flushEvery.first, flushEvery.second)
-                                        .subscribe { scanner.flushPendingScanResults(callback) }
-                            }
-
                             downStream.setCancellable {
-                                flushEveryDisp?.dispose()
+                                disposables.dispose()
                                 scanner.stopScan(callback)
                             }
                         }, BackpressureStrategy.BUFFER)
---
+++ src/main/kotlin/com/vincentmasselis/rxbluetoothkotlin/internal/IntentFilter+rx.kt
@@ -0,0 +1,18 @@
+package com.vincentmasselis.rxbluetoothkotlin.internal
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import io.reactivex.Observable
+
+fun IntentFilter.toObservable(context: Context): Observable<Pair<Context, Intent>> =
+        Observable.create({ downStream ->
+            val receiver = object : BroadcastReceiver() {
+                override fun onReceive(currentContext: Context, intent: Intent) {
+                    downStream.onNext(currentContext to intent)
+                }
+            }
+            context.registerReceiver(receiver, this)
+            downStream.setCancellable { context.unregisterReceiver(receiver) }
+        })
